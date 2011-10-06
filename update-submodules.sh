54957e2ba9bc0212262f398ada00b34162255341
Carl Helmertz <helmertz@gmail.com>
2011-10-06 21:05:59 +0200

    Make it even easier to update submodules

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,10 +1,16 @@
+#!/usr/bin/env bash
 # quick and dirty, pretty static :)
 # beware, this always fetches HEAD and merges on top of master
-BUNDLES_DIR="./.vim/bundle"
-for DIR in `ls $BUNDLES_DIR`
-do
-	pushd "$BUNDLES_DIR/$DIR"
-	git remote update
-	git merge origin/master
-	popd
-done
+
+function update_submodules() {
+	BUNDLES_DIR="./.vim/bundle"
+	for DIR in `ls $BUNDLES_DIR`
+	do
+		pushd "$BUNDLES_DIR/$DIR"
+		git remote update
+		git merge origin/master
+		popd
+	done
+}
+
+git submodule init && git submodule update && update_submodules
