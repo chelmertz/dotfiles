c98f675e09e12776d06c7bc4f953ad32c5e17ed7
Carl Helmertz <helmertz@gmail.com>
2011-12-13 13:45:17 +0100

    Update submodules with less spam and real feedback
    Signed-off-by: Carl Helmertz <helmertz@gmail.com>

--- update-submodules.sh
+++ update-submodules.sh
@@ -2,15 +2,21 @@
 # quick and dirty, pretty static :)
 # beware, this always fetches HEAD and merges on top of master
 
+function init_submodules() {
+	git submodule init 1> /dev/null
+}
+
 function update_submodules() {
+	git submodule update 1> /dev/null
 	BUNDLES_DIR="./.vim/bundle"
 	for DIR in `ls $BUNDLES_DIR`
 	do
-		pushd "$BUNDLES_DIR/$DIR"
-		git remote update
-		git merge origin/master
-		popd
+		pushd "$BUNDLES_DIR/$DIR" 1> /dev/null
+		git remote update 1> /dev/null
+		git merge origin/master 1> /dev/null
+		popd 1> /dev/null
 	done
 }
 
-git submodule init && git submodule update && update_submodules
+[[ $(init_submodules && update_submodules) -eq 0 ]] && RESULT="success" || RESULT="failure"
+echo $RESULT
