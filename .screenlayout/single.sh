569bdeccde733a9109dc315aa6cc5daf2b8084bd
Carl Helmertz <carl.helmertz@elvaco.se>
2017-12-12 18:17:09 +0100

    Update .screenlayout, thanks arandr
    Signed-off-by: Carl Helmertz <carl.helmertz@elvaco.se>

--- .screenlayout/single.sh
+++ .screenlayout/single.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1280x800 --pos 0x0 --rotate normal --output VGA1 --off
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
