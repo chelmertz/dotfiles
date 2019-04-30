0de2549acf16867a22593fcdfc5f9c32b73b88f0
Carl Helmertz <helmertz@gmail.com>
2019-05-01 01:46:43 +0200

    awesome: set neovim to editor

--- rc.lua
+++ rc.lua
@@ -51,7 +51,7 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
-editor = os.getenv("EDITOR") or "editor"
+editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
