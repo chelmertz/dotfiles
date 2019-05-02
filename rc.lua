99e470af83aa261d46de8343c917d5168bd01700
Carl Helmertz <helmertz@gmail.com>
2019-05-02 19:14:24 +0200

    awesome: use butt ugly background

--- rc.lua
+++ rc.lua
@@ -48,6 +48,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.wallpaper = "/home/ch/Dropbox/inspiration/nostromo.png"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
