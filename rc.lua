3d82bd6a42d2dc570e7e0fbfc431b8daaeefb243
Carl Helmertz <helmertz@gmail.com>
2019-05-05 11:28:42 +0200

    awesome: Kill current window with Pause
    It's a key that's easy to find when there's an external keyboard, at
    least. Likely to change.

--- rc.lua
+++ rc.lua
@@ -427,6 +427,8 @@ clientkeys = gears.table.join(
         {description = "toggle fullscreen", group = "client"}),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
+    awful.key({}, "Pause",      function (c) c:kill()                         end,
+              {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
