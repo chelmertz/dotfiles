6e1654e89871f2f95149504680d2c1564ec404f8
Carl Helmertz <helmertz@gmail.com>
2019-05-25 13:37:14 +0200

    awesome: lock screen through terminal, b/c conflicting keys

--- rc.lua
+++ rc.lua
@@ -351,8 +351,6 @@ globalkeys = gears.table.join(
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
-    awful.key({ modkey, "Shift"   }, "l", function() awful.spawn("xscreensaver-command -lock") end,
-               {description = "lock screen", group = "awesome"}),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
               {description = "increase master width factor", group = "layout"}),
