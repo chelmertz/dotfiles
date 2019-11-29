5ea763d6d829a3c8a45ec86945a8578cbe50b566
Carl Helmertz <helmertz@gmail.com>
2019-11-29 20:06:03 +0100

    focus_emacs: first focus emacs, then maximize
    previously the focus was not deterministic (for some reason)

--- bin/focus_emacs.sh
+++ bin/focus_emacs.sh
@@ -3,4 +3,5 @@
 # bind this to F8 or such
 
 pidof emacs >/dev/null || nohup emacs > /dev/null &
-wmctrl -x -a Emacs -b add,maximized_vert,maximized_horz
+wmctrl -x -a Emacs
+wmctrl -r Emacs -b add,maximized_vert,maximized_horz
