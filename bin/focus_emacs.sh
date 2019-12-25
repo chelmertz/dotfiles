a8237ee0e40b2c79f5642134d05faa48c368a82b
Carl Helmertz <helmertz@gmail.com>
2019-12-25 14:30:29 +0100

    emacs process is now called 'emacs26' on ubuntu

--- bin/focus_emacs.sh
+++ bin/focus_emacs.sh
@@ -2,6 +2,6 @@
 
 # bind this to F8 or such
 
-pidof emacs >/dev/null || nohup emacs > /dev/null &
+pidof emacs26 >/dev/null || nohup emacs26 > /dev/null &
 wmctrl -x -a Emacs
 wmctrl -r Emacs -b add,maximized_vert,maximized_horz
