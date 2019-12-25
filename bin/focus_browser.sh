11034de5fea04db884727be93556586cb74c9cbf
Carl Helmertz <helmertz@gmail.com>
2019-12-25 14:30:48 +0100

    switch browser to firefox.. again

--- bin/focus_browser.sh
+++ bin/focus_browser.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # bind this to F4 or such
+process=firefox
+wmclass=Firefox
 
-pidof chrome >/dev/null || nohup /usr/bin/x-www-browser > /dev/null &
-wmctrl -x -a google-chrome
-wmctrl -r google-chrome -b add,maximized_vert,maximized_horz
+pidof $process >/dev/null || nohup /usr/bin/x-www-browser > /dev/null &
+wmctrl -x -a $wmclass
+wmctrl -r $wmclass -b add,maximized_vert,maximized_horz
