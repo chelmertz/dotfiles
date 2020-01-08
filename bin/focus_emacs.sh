560c6458038212368ab41c1b36e10fa4f31e1eda
Carl Helmertz <helmertz@gmail.com>
2020-01-08 01:07:42 +0100

    focus_emacs: Focus emacs by pid, not part of WM_CLASS
    When firefox is viewing a page with the title "bla bla Emacs", that
    window was activated before this patch. Now, we cross reference the pid
    with the window listing (wmctrl -l -p <- list with pid) and use the
    window's id (first column of wmctrl -l -p) to give focus to the actual
    emacs window.
    
    It feels like my shell skills may come around and bite me..

--- bin/focus_emacs.sh
+++ bin/focus_emacs.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+set -x
+set -e
+
 # bind this to F8 or such
 
-pidof emacs26 >/dev/null || nohup emacs26 > /dev/null &
-wmctrl -x -a Emacs
-wmctrl -r Emacs -b add,maximized_vert,maximized_horz
+pidof emacs >/dev/null || ( (nohup emacs >/dev/null &) && sleep 1)
+pid=$(pidof emacs)
+wid=$(wmctrl -l -p | grep emacs | grep "$pid" | cut -d' ' -f1)
+wmctrl -i -a "$wid"
+wmctrl -i -r "$wid" -b add,maximized_vert,maximized_horz
