d4ea9d04453e8ca35edb0253a1504539bf230d0b
Carl Helmertz <helmertz@gmail.com>
2020-01-24 15:57:27 +0100

    focus_emacs: sometimes it's emacs, sometimes emacs26 :(

--- bin/focus_emacs.sh
+++ bin/focus_emacs.sh
@@ -5,8 +5,8 @@ set -e
 
 # bind this to F8 or such
 
-pidof emacs >/dev/null || ( (nohup emacs >/dev/null &) && sleep 1)
-pid=$(pidof emacs)
+pidof emacs >/dev/null || pidof emacs26 >/dev/null || ( (nohup emacs >/dev/null &) && sleep 1)
+pid=$(pidof emacs || pidof emacs26)
 wid=$(wmctrl -l -p | grep emacs | grep "$pid" | cut -d' ' -f1)
 wmctrl -i -a "$wid"
 wmctrl -i -r "$wid" -b add,maximized_vert,maximized_horz
