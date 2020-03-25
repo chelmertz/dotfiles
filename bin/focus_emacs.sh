c38ab1ba79f33b6ca749fe8f9434e81c9d5baabf
Carl Helmertz <helmertz@gmail.com>
2020-03-25 16:17:17 +0100

    focus_emacs: Use emacs in a terminal instead
    It feels like it's much faster.

--- bin/focus_emacs.sh
+++ bin/focus_emacs.sh
@@ -5,8 +5,14 @@ set -e
 
 # bind this to F8 or such
 
-pidof emacs >/dev/null || pidof emacs26 >/dev/null || ( (nohup emacs >/dev/null &) && sleep 1)
-pid=$(pidof emacs || pidof emacs26)
-wid=$(wmctrl -l -p | grep emacs | grep "$pid" | cut -d' ' -f1)
+title="emacsclient - terminal"
+
+start_emacs() {
+  gnome-terminal --title "$title" -- emacsclient -tc ~
+}
+
+pidof emacsclient >/dev/null || start_emacs
+wid=$(wmctrl -l -p | grep "$title" |  cut -d' ' -f1)
+test -z "$wid" && exit 1
 wmctrl -i -a "$wid"
 wmctrl -i -r "$wid" -b add,maximized_vert,maximized_horz
