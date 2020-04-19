000f6050aba447747c39632e06988eb7be76916d
Carl Helmertz <helmertz@gmail.com>
2020-04-20 00:47:24 +0200

    emacs/doom: Bring over config from vanilla emacs
    So far, I really, really like doom. I failed hard to configure
    typescript for hours in vanilla, it took one minute to fix in doom.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -52,6 +52,74 @@
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
 
+
+; from installation notes:
+;
+;1. Whenever you edit your doom! block in ~/.doom.d/init.el or modify your
+;   modules, run:
+;
+;     bin/doom refresh
+;
+;   This will ensure all needed packages are installed, all orphaned packages are
+;   removed, and your autoloads files are up to date. This is important! If you
+;   forget to do this you will get errors!
+;
+;2. If something inexplicably goes wrong, try `bin/doom doctor`
+;
+;   This will diagnose common issues with your environment and setup, and may
+;   give you clues about what is wrong.
+;
+;3. Use `bin/doom upgrade` to update Doom. Doing it any other way may require
+;   additional work. When in doubt, run `bin/doom sync`.
+;
+;4. Check out `bin/doom help` to see what else `bin/doom` can do (and it is
+;   recommended you add ~/.emacs.d/bin to your PATH).
+;
+;5. You can find Doom's documentation via `M-x doom/help` or `SPC h D`.
+
+; doom bindings:
+; list buffers: ,<
+; org capture: ,X
+
+
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-(setq doom-leader-key ",")
 (setq confirm-kill-emacs nil)
+(after! org
+  (setq org-adapt-indentation nil
+        org-startup-indented nil
+        org-export-with-sub-superscripts nil
+        org-capture-templates '(
+                                ("t" "TODO" entry (file "inbox.org") "* TODO %?\n:PROPERTIES:\n:CREATED: %T\n:END:\n\n  %i")
+                                ("j" "Journal" entry (file+datetree "journal.org") "* %?\n:PROPERTIES:\n:CREATED: %T\n:END:\n" :jump-to-captured t)
+                                ("e" "Elvaco TODO" entry (file+headline "elvaco.org" "TODOs")
+                                 "* TODO %?\n:PROPERTIES:\n:CREATED: %T\n:END:\n\n  %i")
+                                ("r" "Elvaco retrospective" entry (file+headline "elvaco.org" "Retrospective")
+                                 "* TODO %?\n:PROPERTIES:\n:CREATED: %T\n:END:\n\n  %i")
+                                )
+        ))
+
+(defun export-html-and-open ()
+  (interactive)
+  (org-open-file (org-html-export-to-html)))
+
+; from https://github.com/abo-abo/oremacs/commit/6c86696c0a1f66bf690e1a934683f85f04c6f34d
+; but slightly modified (to txt instead of html)
+(defun org-to-plaintext-to-clipboard ()
+  "Export region to HTML, and copy it to the clipboard."
+  (interactive)
+  (org-export-to-file 'ascii "/tmp/org.txt")
+  (apply
+   'start-process "xclip" "*xclip*"
+   (split-string
+    "xclip -verbose -i /tmp/org.txt -t text/plain -selection clipboard" " ")))
+
+
+(setq doom-leader-key ",")
+(map! "<f6>" 'projectile-replace)
+(map! :leader "e" (lambda() (interactive) (find-file (concat org-directory "elvaco.org"))))
+(map! :leader "j" (lambda() (interactive) (find-file (concat org-directory "journal.org"))))
+(map! :leader "w" 'kill-buffer-and-window)
+(map! :leader "m" '(lambda() (interactive) (org-capture nil "j")))
+(map! :leader "a" 'org-agenda)
+(map! :map org-mode-map :leader "h" 'export-html-and-open)
+(map! :map org-mode-map :leader "t" 'org-to-plaintext-to-clipboard)
