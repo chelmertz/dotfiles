3c36bf5510de684e053ac2be3e34d8724a48d9ed
Carl Helmertz <helmertz@gmail.com>
2020-04-22 23:41:57 +0200

    bin/focus_emacs: Launch (or focus) doom emacs

--- bin/focus_emacs.sh
+++ bin/focus_emacs.sh
@@ -2,16 +2,8 @@
 
 set -xe
 
-title="emacsclient - terminal"
-
-start_emacs() {
-    gnome-terminal --title "$title" -- emacsclient -tc ~/Dropbox/orgzly/
-    sleep 0.1
-}
+title="Doom Emacs"
 
 wid=$(wmctrl -l -p | grep "$title" |  cut -d' ' -f1)
-test -z "$wid" && start_emacs
-wid=$(wmctrl -l -p | grep "$title" |  cut -d' ' -f1)
-test -z "$wid" && exit 1
-wmctrl -i -a "$wid"
-wmctrl -i -r "$wid" -b add,maximized_vert,maximized_horz
+test -z "$wid" && exec ~/bin/doom run
+wmctrl -i -a "$wid" # -a = focus window
