6793d1b0373c4794d12e5f44c0e9269238322982
Carl Helmertz <helmertz@gmail.com>
2020-05-05 13:55:12 +0200

    focus_browser.sh is superseded, remove
    Use `switch_application_window.sh Firefox firefox` instead

--- bin/focus_browser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# bind this to F4 or such
-process=firefox
-wmclass=Firefox
-
-pidof $process >/dev/null || nohup /usr/bin/x-www-browser > /dev/null &
-wmctrl -x -a $wmclass
-wmctrl -r $wmclass -b add,maximized_vert,maximized_horz
