574ce7ec37f31cd9da7f428e16b96b4cac12b4f0
Carl Helmertz <helmertz@gmail.com>
2020-06-23 23:46:34 +0200

    rofi: snippet (super+s) & emoji (super+e) support
    Also: remove any reference to alacritty, which was
    slow/feature-incomplete for me, compared to the good old gnome-terminal.

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-modi="run,ssh,emoji:$HOME/code/github/nkoehring/rofiemoji/rofiemoji.sh,snippets:$HOME/snippets.sh" 
+snippets="snippets:$HOME/code/github/chelmertz/rofi-blah-blah/rofi-blah-blah"
+emoji="emoji:$HOME/code/github/nkoehring/rofiemoji/rofiemoji.sh"
+
+modi="run,ssh,$emoji,$snippets"
 
 exec rofi -sidebar-mode -show combi -combi-modi "$modi" -matching fuzzy -modi "$modi" -theme-str '#prompt {enabled: false; }'
--- bin/rofi_snippets.sh
+++ bin/rofi_snippets.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-modi="snippets:$HOME/snippets.sh" 
+modi="snippets:$HOME/code/github/chelmertz/rofi-blah-blah/rofi-blah-blah"
 
 exec rofi -show snippets -matching fuzzy -modi "$modi" -theme-str '#prompt {enabled: false; }'
