15c53957d141329eed1167b5ba01dc7367808657
Carl Helmertz <helmertz@gmail.com>
2020-07-26 22:26:48 +0200

    focus_emacs: Refer to newly installed location

--- bin/focus_emacs.sh
+++ bin/focus_emacs.sh
@@ -5,5 +5,5 @@ set -xe
 title="Doom Emacs"
 
 wid=$(wmctrl -l -p | grep "$title" |  cut -d' ' -f1)
-test -z "$wid" && exec ~/bin/doom run
+test -z "$wid" && exec /home/ch/.emacs.d/bin/doom run
 wmctrl -i -a "$wid" # -a = focus window
