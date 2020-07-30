0d58af0ef6a244a2599ff339e3562126c0aa1625
Carl Helmertz <helmertz@gmail.com>
2020-07-30 21:36:16 +0200

    Add a timer ($mod+t)
    To allow for cancelling, I needed to add `--block` and
    `--timeout`, instead of sleeping in the main loop, otherwise I could not
    trap the exit "signal" of dunstify.
    
    I want to allow cancelling by ctrl+space or just left-clicking the
    notification window, but after having closed that notification, the loop
    continued.
    
    This would work alright, but there's a flicker for each update, which is
    super annoying.

---
+++ bin/rofi_timer.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -euo pipefail
+#set -x
+
+seconds=$(echo -e "10\n30\n60\n1500" | rofi -dmenu -width 20 -lines 4 -p "Set a timer for X seconds")
+initial=$seconds
+
+format_seconds() {
+    date +%T -d "1970-01-01 + $1 seconds"
+}
+
+[ "$seconds" -lt 1 ] && exit 1
+an_id="$RANDOM"
+name=${1:-default}
+
+# TODO dynamic icon/colored circle, representing time left
+d() {
+    secs=$1
+    urg=normal
+    if [ "$seconds" -lt 6 ]; then
+        urg=critical
+        paplay /usr/share/sounds/freedesktop/stereo/bell.oga
+    fi
+    sec_string=$(format_seconds "$secs")
+    code=$(dunstify --appname "Timer" --urgency="$urg" --timeout 1000 --block --replace="$an_id" "$name $sec_string")
+    if [ "$code" = 2 ]; then
+        dunstify --close="$an_id"
+        exit
+    fi
+}
+
+d "$seconds"
+
+while [[ $seconds -gt 0 ]]; do
+    #sleep 1
+    seconds=$((seconds - 1))
+    if [ "$seconds" -eq 0 ]; then
+        dunstify --appname "Timer" --replace="$an_id" --urgency=critical "Timer '$name' done, $initial seconds passed" "$(date)"
+        paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+        if [ "$name" != "default" ]; then
+            spd-say "$name"
+        fi
+    else
+        d $seconds
+    fi
+done
+
