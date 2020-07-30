94b8cca4f95ad4fd73b040521570315771a176b8
Carl Helmertz <helmertz@gmail.com>
2020-07-30 21:49:23 +0200

    timer: Allow user to name the timer

--- bin/rofi_timer.sh
+++ bin/rofi_timer.sh
@@ -11,7 +11,7 @@ format_seconds() {
 
 [ "$seconds" -lt 1 ] && exit 1
 an_id="$RANDOM"
-name=${1:-default}
+timer_name=${1:-$(echo "default" | rofi -dmenu -width 20 -l 1 -p "Name your timer")}
 
 # TODO dynamic icon/colored circle, representing time left
 d() {
@@ -22,7 +22,7 @@ d() {
         paplay /usr/share/sounds/freedesktop/stereo/bell.oga
     fi
     sec_string=$(format_seconds "$secs")
-    code=$(dunstify --appname "Timer" --urgency="$urg" --timeout 1000 --block --replace="$an_id" "$name $sec_string")
+    code=$(dunstify --appname "Timer" --urgency="$urg" --timeout 1000 --block --replace="$an_id" "$timer_name $sec_string")
     if [ "$code" = 2 ]; then
         dunstify --close="$an_id"
         exit
@@ -35,10 +35,10 @@ while [[ $seconds -gt 0 ]]; do
     #sleep 1
     seconds=$((seconds - 1))
     if [ "$seconds" -eq 0 ]; then
-        dunstify --appname "Timer" --replace="$an_id" --urgency=critical "Timer '$name' done, $initial seconds passed" "$(date)"
+        dunstify --appname "Timer" --replace="$an_id" --urgency=critical "Timer '$timer_name' done, $initial seconds passed" "$(date)"
         paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-        if [ "$name" != "default" ]; then
-            spd-say "$name"
+        if [ "$timer_name" != "default" ]; then
+            spd-say "$timer_name"
         fi
     else
         d $seconds
