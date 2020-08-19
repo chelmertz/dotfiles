61dab6020bb505b2d1ad32c0213827ef1b99c9c9
Carl Helmertz <helmertz@gmail.com>
2020-08-19 12:43:41 +0200

    rofi_power: Lowercase alternatives, easier to type

--- bin/rofi_power.sh
+++ bin/rofi_power.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cmd=$(echo -e "Lock screen\nShutdown\nReboot" | rofi -p 'Sesssion action' -dmenu)
+cmd=$(echo -e "lock screen\nshutdown\nreboot" | rofi -p 'Sesssion action' -dmenu)
 
 case $cmd in
-    "Lock screen")
+    "lock screen")
         i3lock -c 000000
         ;;
-    Shutdown)
+    shutdown)
         poweroff
         ;;
-    Reboot)
+    reboot)
         reboot
         ;;
 esac
