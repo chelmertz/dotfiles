50a7df3a38d0193e8880f05b946626cc2096aa74
Carl Helmertz <helmertz@gmail.com>
2021-01-04 00:42:59 +0100

    text to opml.py
    Trying out Fraidyc.at, so I needed an OPML. I liked elfeed but I ended
    up ignoring the feed reader since... it's just too much info. Maybe
    fraidycat is the salvation.

---
+++ bin/text-to-opml.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+from xml.sax.saxutils import quoteattr
+
+print("""
+<opml version="1.0">
+<body>
+""")
+for line in sys.stdin.readlines():
+    if 'http' in line:
+        url = line.split(' ')[1]
+        attr = quoteattr(url)
+        print("<outline text={} xmlUrl={}/>".format(attr, attr))
+
+    
+# this expects something like
+# cat ~/Dropbox/orgzly/feeds.org
+# as input, where one line can be something like
+# *** http://asdf.com/feed
+
+print(
+    """</body>
+</opml>
+""")
