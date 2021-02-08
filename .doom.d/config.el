121cd7b8b4b63fb4bc38f77a31eee89d5a2a1abc
Carl Helmertz <helmertz@gmail.com>
2021-02-08 09:22:21 +0100

    doom/emacs: hint about reloading config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; reload with M-x eval-buffer RET
+
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
