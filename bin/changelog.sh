5e02e17790003a13444fbb5b5e7343dcd0a47e8c
Carl Helmertz <helmertz@gmail.com>
2021-02-10 18:22:47 +0100

    changelog.sh: Basic changelog "since last tag"

---
+++ bin/changelog.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git fetch --all 1>/dev/null 2>&1
+last_tag=$(git tag --sort=-v:refname | head -n1)
+default_remote_ref=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's|refs/remotes/||')
+echo "comparing latest tag ($last_tag) with $default_remote_ref" 1>&2
+git log --no-merges --pretty=tformat:%s "$last_tag".."$default_remote_ref" | sed 's/^/- /'
