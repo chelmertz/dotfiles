9af0fc03c7c458dee815531d184d33df7a101ad7
Carl Helmertz <helmertz@gmail.com>
2021-02-26 12:56:25 +0100

    changelog: Exclude commits starting with cleanup:

--- bin/changelog.sh
+++ bin/changelog.sh
@@ -3,4 +3,4 @@ git fetch --all 1>/dev/null 2>&1
 last_tag=$(git tag --sort=-v:refname | head -n1)
 default_remote_ref=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's|refs/remotes/||')
 echo "comparing latest tag ($last_tag) with $default_remote_ref" 1>&2
-git log --no-merges --pretty=tformat:%s "$last_tag".."$default_remote_ref" | sed 's/^/- /'
+git log --no-merges --pretty=tformat:%s "$last_tag".."$default_remote_ref" | sed -e '/^cleanup:/d' -e 's/^/- /'
