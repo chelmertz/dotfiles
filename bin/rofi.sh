95a0929c85967d56d42de414536ea139d704f528
Carl Helmertz <helmertz@gmail.com>
2021-02-26 11:13:09 +0100

    rofi: ssh on ctrl+shift+tab
    Checking a lot of stuff on servers nowadays :/

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 snippets="snippets:$HOME/code/github/chelmertz/rofi-blah-blah/rofi-blah-blah"
 emoji="emoji:$HOME/code/github/nkoehring/rofiemoji/rofiemoji.sh"
 
-modi="run,ssh,$emoji,$snippets"
+modi="run,$emoji,$snippets,ssh"
 
 exec rofi -sidebar-mode -show combi -combi-modi "$modi" -matching fuzzy -modi "$modi" -theme-str '#prompt {enabled: false; }'
