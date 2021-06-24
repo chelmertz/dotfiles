99c963631a292195ec7bd4c84990599c8b24d348
Carl Helmertz <helmertz@gmail.com>
2021-06-24 18:08:17 +0200

    i3blocks_date: Firefox -> Chrome
    Not for the final time I guess. 10 years from now, I will have a script
    outputting "current browser's x window class".

--- bin/i3blocks_date.sh
+++ bin/i3blocks_date.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-[[ "$BLOCK_BUTTON" -eq 1 ]] && xdg-open "https://calendar.google.com" && wmctrl -a Firefox
+[[ "$BLOCK_BUTTON" -eq 1 ]] && xdg-open "https://calendar.google.com" &> /dev/null && wmctrl -a Google-chrome
 date '+%A 📅 %Y-%m-%d (week %V) 🕞 %H:%M:%S'
