12e513a0a960b9258cf006d4863a13c50f4a7a3d
Carl Helmertz <helmertz@gmail.com>
2021-07-02 00:38:16 +0200

    rofi_power: Add suspend & hibernate
    Together with some hints, since I always confuse hibernate with suspend.

--- bin/rofi_power.sh
+++ bin/rofi_power.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cmd=$(echo -e "lock screen\nshutdown\nreboot\nlog out" | rofi -p 'Session action' -dmenu)
+cmd=$(echo -e "lock screen\nshutdown\nsuspend (low power)\nhibernate (memory -> swap, power off)\nreboot\nlog out" | rofi -p 'Session action' -dmenu)
+
+lock() {
+    i3lock -c 000000 --ignore-empty-password --show-failed-attempts
+}
 
 case $cmd in
-    "lock screen")
-        i3lock -c 000000
+    lock*)
+        lock
         ;;
     shutdown)
         poweroff
+        ;;
+    suspend*)
+        lock
+        systemctl suspend
+        ;;
+    hibernate*)
+        systemctl hibernate
         ;;
     reboot)
         reboot
