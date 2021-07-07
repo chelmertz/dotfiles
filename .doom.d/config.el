7e94f4c0fbfcc22cd74776f5a8d44a885cc00bac
Carl Helmertz <helmertz@gmail.com>
2021-07-08 00:02:39 +0200

    emacs/doom: Switch to light theme
    It's summer time! Sitting outside with a dark background is a pita.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -26,7 +26,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;(setq doom-theme 'doom-one) ; dark, default
+(setq doom-theme 'doom-acario-light) ; light
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
