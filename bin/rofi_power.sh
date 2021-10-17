54669fb66d61ee843a078a24102628b3806da2f7
Carl Helmertz <helmertz@gmail.com>
2021-10-17 11:17:38 +0200

    rofi_power: Remove hibernate
    It's not supported on my machine (omega, the Dell Inspiron Ubuntu)
    through systemctl at least.

--- bin/rofi_power.sh
+++ bin/rofi_power.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cmd=$(echo -e "lock screen\nshutdown\nsuspend (low power)\nhibernate (memory -> swap, power off)\nreboot\nlog out" | rofi -p 'Session action' -dmenu)
+cmd=$(echo -e "lock screen\nshutdown\nsuspend (low power)\nreboot\nlog out" | rofi -p 'Session action' -dmenu)
 
 lock() {
     i3lock -c 000000 --ignore-empty-password --show-failed-attempts
@@ -17,9 +17,6 @@ case $cmd in
     suspend*)
         lock
         systemctl suspend
-        ;;
-    hibernate*)
-        systemctl hibernate
         ;;
     reboot)
         reboot
