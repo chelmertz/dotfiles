9f187279b23758802feb923ad1c1434c0b74d3df
Calle Helmertz <helmertz@gmail.com>
2021-10-26 16:53:09 +0200

    i3blocks_date: Don't display output in i3blocks

--- bin/i3blocks_date.sh
+++ bin/i3blocks_date.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-[[ "$BLOCK_BUTTON" -eq 1 ]] && xdg-open "https://calendar.google.com" &>/dev/null && i3-msg workspace number 2 && wmctrl -a Google-chrome
+[[ "$BLOCK_BUTTON" -eq 1 ]] && xdg-open "https://calendar.google.com" &>/dev/null && i3-msg workspace number 2 &>/dev/null && wmctrl -a Google-chrome &>/dev/null
 date '+%A 📅 %Y-%m-%d (week %V) 🕞 %H:%M:%S'
