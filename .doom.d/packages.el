1f30e15bed648ecef1da538839ddae20121c0ff1
Carl Helmertz <helmertz@gmail.com>
2022-01-03 21:24:12 +0100

    doom emacs: Add exec-path-from-shell
    I'm not sure why, but gopls is in my $PATH but still not recognized by
    the Golang LSP from within Emacs. exec-path-from-shell modifies emacs'
    "variant of $PATH" (probably very wrong wording) to keep it in sync with
    what's in my shell.

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -11,6 +11,8 @@
 
 (package! command-log-mode)
 
+(package! exec-path-from-shell)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
