9d4c4d806c1a8953df8e4bb3b130c4a4d30d5965
Calle Helmertz <helmertz@gmail.com>
2022-06-22 15:25:19 +0200

    ansible/org mode: mermaid cli for drawing diagram

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -13,6 +13,8 @@
 
 (package! exec-path-from-shell)
 
+(package! mermaid-mode)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
