49ed45dab913ff5f8924341ba0bd374e35908c75
Calle Helmertz <helmertz@gmail.com>
2023-08-14 21:48:50 +0200

    i3: Change default browser to firefox again
    No particular reason for changing right now.

--- bin/i3blocks_date.sh
+++ bin/i3blocks_date.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-[[ "$BLOCK_BUTTON" -eq 1 ]] && xdg-open "https://calendar.google.com" &>/dev/null && i3-msg workspace number 2 &>/dev/null && wmctrl -a Google-chrome &>/dev/null
+[[ "$BLOCK_BUTTON" -eq 1 ]] && xdg-open "https://calendar.google.com" &>/dev/null && i3-msg workspace number 2 &>/dev/null && wmctrl -a firefox &>/dev/null
 date '+📅 %A %d %b w:%V 🕞 %H:%M'
