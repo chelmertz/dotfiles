8034e687dca33967d7211a52844e7f4e46477c5e
Calle Helmertz <helmertz@gmail.com>
2024-03-26 00:54:09 +0100

    ansible: Be a bit more verbose in the output

--- bin/apply-ansible.sh
+++ bin/apply-ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-exec ansible-playbook --connection=local --ask-become-pass ~/code/github/chelmertz/dotfiles/ansible-laptop.yml
+exec ansible-playbook --verbose --connection=local --ask-become-pass ~/code/github/chelmertz/dotfiles/ansible-laptop.yml
 
