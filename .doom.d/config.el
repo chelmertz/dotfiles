0b121d4b959cf7df8cf9c17b30190d662a54ceb2
Calle Helmertz <helmertz@gmail.com>
2024-06-27 09:23:07 +0200

    doom: Focus firefox after exporting to html

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -106,12 +106,14 @@
                                 )
         ))
 (remove-hook 'doom-first-input-hook #'evil-snipe-mode)
+
+
 (defun export-html-and-open ()
   (interactive)
   (if (derived-mode-p 'org-mode)
       (org-open-file (org-html-export-to-html) 'system)
     (browse-url-of-buffer))
-  (shell-command "wmctrl -a chrome"))
+  (shell-command "wmctrl -a firefox"))
 
 ; from https://github.com/abo-abo/oremacs/commit/6c86696c0a1f66bf690e1a934683f85f04c6f34d
 ; but slightly modified (to txt instead of html)
