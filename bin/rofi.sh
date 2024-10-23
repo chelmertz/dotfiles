304e4495992295ebf27937ff7cf586c58f15c599
Calle Helmertz <helmertz@gmail.com>
2024-10-23 17:54:23 +0200

    Remove snippets-through-rofi, use espanso instead
    The snippets via rofi never really worked well, since rofi was
    in the foreground, the text was inserted in a weird place
    when coming back to the process with the text input field.

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -2,9 +2,8 @@
 set -euo pipefail
 
 settings="settings:settings_rofi.sh"
-snippets="snippets:$HOME/code/github/chelmertz/rofi-blah-blah/rofi-blah-blah"
 emoji="emoji:$HOME/code/github/nkoehring/rofiemoji/rofiemoji.sh"
 
-modi="run,$settings,$emoji,$snippets,ssh"
+modi="run,$settings,$emoji,ssh"
 
 exec rofi -sidebar-mode -show combi -combi-modi "$modi" -matching fuzzy -modi "$modi" -theme-str '#prompt {enabled: false; }'
--- bin/rofi_snippets.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-modi="snippets:$HOME/code/github/chelmertz/rofi-blah-blah/rofi-blah-blah"
-
-exec rofi -show snippets -matching fuzzy -modi "$modi" -theme-str '#prompt {enabled: false; }'
