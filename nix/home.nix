3fea66b09bb9011867bb119d4ab5a874d864422b
Calle Helmertz <helmertz@gmail.com>
2026-01-01 14:15:16 +0000

    bright: allow a darker display

--- nix/home.nix
+++ nix/home.nix
@@ -5,6 +5,7 @@
   home.stateVersion = "24.05";
 
   home.packages = with pkgs; [
+    # Already migrated
     bat
     cloc
     fd
@@ -22,6 +23,57 @@
     tree
     vlc
     w3m
+
+    # CLI tools
+    htop
+    entr
+    sqlite
+    nmap
+    pandoc
+    graphviz
+    visidata
+    asciinema
+    figlet
+    html-tidy
+    redshift
+
+    # GUI tools
+    copyq
+    pavucontrol
+    arandr
+    autorandr
+    sqlitebrowser
+    rofi
+    flameshot
+
+    # X11/i3 tools
+    i3blocks
+    xcape
+    xclip
+    xsel
+    xdotool
+    wmctrl
+    brightnessctl
+    playerctl
+
+    # pip replacements
+    litecli
+    pgcli
+    git-filter-repo
+
+    # go replacements
+    lazygit
+    yq
+    lazydocker
+    gopls
+    shfmt
+    uni
+    runme
+
+    # cargo + misc
+    ripdrag
+    php
+    font-awesome
   ];
 
   programs.home-manager.enable = true;
