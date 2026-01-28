43610af38f44e6fa727884175f1b975a4c11e706
Calle Helmertz <helmertz@gmail.com>
2026-01-28 23:40:27 +0000

    move dunst & config (defaults, probably) to nix

--- nix/home.nix
+++ nix/home.nix
@@ -183,6 +183,87 @@
     "Xcursor.size" = 24;
   };
 
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        monitor = 0;
+        follow = "keyboard";
+        geometry = "400x5-30+20";
+        width = 300;
+        height = 300;
+        origin = "top-right";
+        offset = "10x50";
+        scale = 0;
+        notification_limit = 0;
+        progress_bar = true;
+        progress_bar_height = 10;
+        progress_bar_frame_width = 1;
+        progress_bar_min_width = 150;
+        progress_bar_max_width = 300;
+        indicate_hidden = true;
+        transparency = 0;
+        separator_height = 2;
+        padding = 16;
+        horizontal_padding = 12;
+        text_icon_padding = 0;
+        frame_width = 0;
+        frame_color = "#aaaaaa";
+        separator_color = "frame";
+        sort = true;
+        font = "Monospace 12";
+        line_height = 0;
+        markup = "full";
+        format = "<b>%s</b>\\n%b";
+        alignment = "left";
+        vertical_alignment = "center";
+        show_age_threshold = 60;
+        ellipsize = "middle";
+        ignore_newline = false;
+        stack_duplicates = true;
+        hide_duplicate_count = false;
+        show_indicators = true;
+        icon_position = "left";
+        min_icon_size = 0;
+        max_icon_size = 32;
+        icon_path = "/usr/share/icons/gnome/16x16/status/:/usr/share/icons/gnome/16x16/devices/";
+        sticky_history = true;
+        history_length = 20;
+        dmenu = "rofi -dmenu -p dunst";
+        browser = "/usr/bin/xdg-open";
+        always_run_script = true;
+        title = "Dunst";
+        class = "Dunst";
+        corner_radius = 6;
+        ignore_dbusclose = false;
+        force_xwayland = false;
+        force_xinerama = false;
+        mouse_left_click = "close_current";
+        mouse_middle_click = "do_action, close_current";
+        mouse_right_click = "close_all";
+      };
+      experimental = {
+        per_monitor_dpi = false;
+      };
+      urgency_low = {
+        background = "#222222";
+        foreground = "#888888";
+        timeout = 10;
+      };
+      urgency_normal = {
+        background = "#41484d";
+        foreground = "#ffffff";
+        timeout = 10;
+      };
+      urgency_critical = {
+        background = "#900000";
+        foreground = "#ffffff";
+        frame_color = "#ff0000";
+        timeout = 0;
+      };
+    };
+  };
+
   xdg.configFile."prometheus/prometheus.yml".text = ''
     global:
       scrape_interval: 1m
