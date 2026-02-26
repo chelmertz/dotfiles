97e9ca3dedce19017b92121c5a9bc638da7cbdfb
Calle Helmertz <helmertz@gmail.com>
2026-02-26 21:17:37 +0000

    rofi: "run" to the left prioritizes it over emojis and such

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -5,6 +5,6 @@ settings="settings:settings_rofi.sh"
 emoji="emoji:rofimoji --action copy"
 
 projects="projects:i3-project rofi-modi"
-modi="$settings,$emoji,$projects,run,combi"
+modi="run,$settings,$emoji,$projects,combi"
 
 exec rofi -sidebar-mode -show combi -combi-modi "$modi" -matching fuzzy -modi "$modi" -theme-str '#prompt {enabled: false; }'
