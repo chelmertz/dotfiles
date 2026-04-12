0435104cf75704d7c8d6964cce26f84d8d242a8a
Calle Helmertz <helmertz@gmail.com>
2026-04-12 19:21:49 +0000

    rofi: sort matches
    Finds exact matches, like `coffin` for an emoji, and ranks it higher
    than just any partial match. This is a bit of a trial.

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -7,4 +7,4 @@ emoji="emoji:rofimoji_copy.sh"
 projects="projects:i3-project rofi-modi"
 modi="run,$settings,$emoji,$projects,combi"
 
-exec rofi -sidebar-mode -show combi -combi-modi "$modi" -matching fuzzy -modi "$modi" -theme-str '#prompt {enabled: false; }'
+exec rofi -sidebar-mode -show combi -combi-modi "$modi" -matching fuzzy -sort -modi "$modi" -theme-str '#prompt {enabled: false; }'
