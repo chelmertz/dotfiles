30137266e532b5936b81cbd7120d509ddb810ca3
Calle Helmertz <helmertz@gmail.com>
2026-05-22 07:53:30 +0000

    add debugging script for when screenlayout (xrandr) fails to meet intent

--- .screenlayout/home_dual.sh
+++ .screenlayout/home_dual.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+. "$(dirname "$0")/_check.sh"
 xrandr --output eDP-1 --mode 1920x1200 --pos 3440x240 --rotate normal --output DP-1 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off
+check_layout eDP-1:1920x1200 DP-1:3440x1440 DP-2:off DP-3:off
